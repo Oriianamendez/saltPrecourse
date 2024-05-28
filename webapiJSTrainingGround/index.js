6ff73b4d7eb46bb1b47a66c8eda274120f740878
Oriana Mendez <egleemendez@gmail.com>
2024-05-28 23:08:20 +0200

    Created new files and updated index.js

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,11 +1,22 @@
-console.log("Finished");
-
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  console.log(`The URL for the request was '${req.url}'`);
+  console.log(`The Method for the request was '${req.method}'`);
+
+  const getFileContentOr404 = (fileName) => {
+    if (!fs.existsSync(`./static/${fileName}`)) {
+      fileName = "404.html";
+    }
+    return fs.readFileSync(`./static/${fileName}`, "utf-8");
+  };
+  const fileName = fileNameOfUrl(req.url);
+  const content = getFileContentOr404(fileName);
+
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("<h1>Hello fellow developer!</h1>");
+  res.setHeader("Content-Type", "text/html");
+  res.end(content);
 });
 
 const hostname = "localhost";
@@ -14,3 +25,13 @@ const port = 3000;
 server.listen(port, hostname, () => {
   console.log(`Server running att http://${hostname}:${port}/`);
 });
+
+const fileNameOfUrl = (url) => {
+  let fileName = "";
+  if (url.split("/")[1] === "") {
+    fileName = "index.html";
+  } else {
+    fileName = url.split("/")[1];
+  }
+  return fileName;
+};
