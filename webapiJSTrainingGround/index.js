8ef88bc709bed20b265f2827dee455a0c81e4634
Oriana Mendez <egleemendez@gmail.com>
2024-05-28 23:42:47 +0200

    Modified index.js

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,37 +1,11 @@
-const http = require("http");
-const fs = require("fs");
+const espress = require("express");
+const app = espress();
 
-const server = http.createServer((req, res) => {
-  console.log(`The URL for the request was '${req.url}'`);
-  console.log(`The Method for the request was '${req.method}'`);
-
-  const getFileContentOr404 = (fileName) => {
-    if (!fs.existsSync(`./static/${fileName}`)) {
-      fileName = "404.html";
-    }
-    return fs.readFileSync(`./static/${fileName}`, "utf-8");
-  };
-  const fileName = fileNameOfUrl(req.url);
-  const content = getFileContentOr404(fileName);
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(content);
+app.get("/", (req, res) => {
+  res.send("Hello fellow developer!");
 });
 
-const hostname = "localhost";
 const port = 3000;
-
-server.listen(port, hostname, () => {
-  console.log(`Server running att http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
-const fileNameOfUrl = (url) => {
-  let fileName = "";
-  if (url.split("/")[1] === "") {
-    fileName = "index.html";
-  } else {
-    fileName = url.split("/")[1];
-  }
-  return fileName;
-};
