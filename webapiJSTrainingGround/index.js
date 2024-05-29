0f4b093f45eaef5598538aaf2fcad7a8b9b0c95d
Oriana Mendez <egleemendez@gmail.com>
2024-05-29 16:21:49 +0200

    Created a new file and updated index.js

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,15 +1,16 @@
-const morgan = require("morgan");
 const express = require("express");
 const app = express();
 
-app.use(morgan("tiny"));
-app.use(express.static("static"));
-
 app.get("/", (req, res) => {
-  res.send("Hello fellow developer!");
+  const developer = {
+    name: "Oriana",
+    email: "unemail@salt.com",
+    city: "Stockholm",
+  };
+  res.status(201).setHeader("location", `/api/developers/1`).json(developer);
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running att http://localhost:${port}/`);
 });
