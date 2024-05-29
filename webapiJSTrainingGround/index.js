a90eb041000acdaded2d6cbded25d72ad672af0b
Oriana Mendez <egleemendez@gmail.com>
2024-05-29 20:03:01 +0200

    Updated index.js

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  const developer = {
+const db = [
+  {
+    id: 1,
     name: "Oriana",
     email: "unemail@salt.com",
     city: "Stockholm",
-  };
-  res.status(201).setHeader("location", `/api/developers/1`).json(developer);
+  },
+  {
+    id: 2,
+    name: "Amin",
+    email: "theemailofamin@salt.com",
+    city: "Buenos Aires",
+  },
+];
+
+app.get("/api/developers/:id", (req, res) => {
+  const developer = db.find((developer) => developer.id == req.params.id);
+  return !developer ? res.status(404).end() : res.json(developer);
 });
 
 const port = 3000;
