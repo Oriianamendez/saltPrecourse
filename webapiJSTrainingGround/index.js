987bf97b17800a51228b288531c46d44c2aa512c
Oriana Mendez <egleemendez@gmail.com>
2024-05-29 13:11:39 +0200

    Updated files

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,5 +1,9 @@
-const espress = require("express");
-const app = espress();
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.static("static"));
 
 app.get("/", (req, res) => {
   res.send("Hello fellow developer!");
